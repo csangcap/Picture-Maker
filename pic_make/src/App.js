3c1251f172c0e4732a503f678aee769f8c41009a
csangcap <csangcap405@gmail.com>
2021-07-01 21:24:53 -0400

    code cleanup
    made a generic handleInputChange method to avoid making too much code.

--- pic_make/src/App.js
+++ pic_make/src/App.js
@@ -6,74 +6,24 @@ class App extends Component{
   constructor(props) {
     super(props);
     this.state = {
-      pic_info: {
-        img_src: "",
-        img_quote: "",
-        img_title: "",
-        img_bc1: props.backColor1,
-      }
-    }
+      img_link: "",
+      book_title: "",
+      book_quote: "",
+      n_colors: 0,
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
   }
 
-  handleSourceChange(event) {
-    //get the current info
-    var pic_info = this.state.pic_info;
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
 
-    //get the modified value
-    var modVal = event.target.value;
-    //console.log(modVal);
-    
-    //set the state
-    pic_info.img_src = modVal;
+    //console.log(target, name, value);
 
     this.setState({
-      pic_info: pic_info
-    }); 
-  }
-
-  handleQuoteChange(event) {
-    //get the current info
-    var pic_info = this.state.pic_info;
-
-    //get the modified value
-    var modVal = event.target.value;
-    
-    //set the state
-    pic_info.img_quote = modVal;
-
-    this.setState({
-      pic_info: pic_info
-    });
-  }
-
-  handleTitleChange(event) {
-    //get the current info
-    var pic_info = this.state.pic_info;
-
-    //get the modified value
-    var modVal = event.target.value;
-    console.log("Title",modVal);
-    
-    //set the state
-    pic_info.img_title = modVal;
-
-    this.setState({
-      pic_info: pic_info
-    });
-  }
-  handleBackChange(event) {
-    //get the current info
-    var pic_info = this.state.pic_info;
-
-    //get the modified value
-    var modVal = event.target.value;
-    //console.log("Title",modVal);
-    
-    //set the state
-    pic_info.img_bc1 = modVal;
-
-    this.setState({
-      pic_info: pic_info
+      [name]: value
     });
   }
 
@@ -84,31 +34,31 @@ class App extends Component{
           <br/>
           <label>
             Link:
-            <input type="url" name="link" value={this.state.pic_info.img_src} onChange={this.handleSourceChange.bind(this)}/>
+            <input type="url" name="img_link" value={this.state.img_link} onChange={this.handleInputChange}/>
           </label>
           <br/>
           <label>
             Book Title:
-            <input type="text" name="title" value={this.state.pic_info.img_title} onChange={this.handleTitleChange.bind(this)}/>
+            <input type="text" name="book_title" value={this.state.book_title} onChange={this.handleInputChange}/>
           </label>
           <br/>
           <label>
             Quote:
-            <input type="text" name="quote" value={this.state.pic_info.img_quote} onChange={this.handleQuoteChange.bind(this)}/>
+            <input type="text" name="book_quote" value={this.state.book_quote} onChange={this.handleInputChange}/>
           </label>
           <br/>
           <label>
             Number of colors:
-            <input type="number" name="colors" min="1" max="10"/>
+            <input type="number" name="n_colors" min="1" max="10" value={this.state.n_colors} onChange={this.handleInputChange}/>
           </label>
         </form>
 
         <div class="container">
           <div class="image">
-            <img src={this.state.pic_info.img_src} alt={this.state.pic_info.img_title}/>
+            <img src={this.state.img_link} alt={this.state.book_title}/>
           </div>
           <div class="text"style={{ background: "linear-gradient(red 0%, orange 20%, yellow 50%, green 75%, blue 100%)" }}>
-            <h1 >{this.state.pic_info.img_quote}</h1>
+            <h1 >{this.state.book_quote}</h1>
           </div>
         </div>
       </div>
